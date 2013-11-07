cab354511560b7f827a8f9474d0e7b8c171bf92c
olomunyak <bongwon.kwon@gmail.com>
2013-11-07 13:21:58 +0900

    create login.jsp page.

--- SpringNote/src/com/uniwill/lms/web/login/controller/LoginController.java
+++ SpringNote/src/com/uniwill/lms/web/login/controller/LoginController.java
@@ -1,7 +1,12 @@
 package com.uniwill.lms.web.login.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.uniwill.lms.web.login.service.ILoginService;
 
@@ -10,4 +15,14 @@ public class LoginController {
 
 	@Autowired
 	public ILoginService iLoginService;
+	
+	@RequestMapping("/login")
+	public ModelAndView login(HttpServletRequest request,
+			                  HttpServletResponse response,
+			                  ModelAndView modelAndView) {
+		
+		modelAndView.setViewName("login/login");
+		
+		return modelAndView;
+	}
 }
