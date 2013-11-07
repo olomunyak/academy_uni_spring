926e4ed20c8e837d5a802ad27b55ad237d6709a3
olomunyak <bongwon.kwon@gmail.com>
2013-11-07 12:54:16 +0900

    delete login

--- SpringNote/src/com/uniwill/lms/web/login/dao/ILoginDao.java
+++
@@ -1,9 +0,0 @@
-package com.uniwill.lms.web.login.dao;
-
-import com.uniwill.lms.web.login.bean.LoginBean;
-
-public interface ILoginDao {
-
-	public LoginBean getUserInfo(LoginBean lb) throws Throwable;
-
-}
--- SpringNote/src/com/uniwill/lms/web/login/service/ILoginService.java
+++
@@ -1,9 +0,0 @@
-package com.uniwill.lms.web.login.service;
-
-import com.uniwill.lms.web.login.bean.LoginBean;
-
-public interface ILoginService {
-
-	public LoginBean getUserInfo(LoginBean lb) throws Throwable;
-
-}
